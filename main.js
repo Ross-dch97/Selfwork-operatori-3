cdd54fb7fceeab53277ee9d7a8b79d1579a1f241
Ross-dch97 <dchross97@outlook.it>
2024-10-28 19:53:43 +0100

    Selfwork-operatori-3

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let num1 = prompt('Inserisci il primo numero:');
+let num2 = prompt('Inserisci il secondo numero:');
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2;
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazione}, moltiplicazione ${moltiplicazione}, divisione ${divisione}, potenza ${potenza}.`);
+
